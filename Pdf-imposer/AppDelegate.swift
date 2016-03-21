fd218454eb959f1696d91ed5869f119057f706f4
Weicheng Zhu <qluoluo@icloud.com>
2016-03-21 15:20:58 -0500

    Initial Commit

---
+++ Pdf-imposer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pdf-imposer
+//
+//  Created by Weicheng Zhu on 3/21/16.
+//  Copyright © 2016 Weicheng Zhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
