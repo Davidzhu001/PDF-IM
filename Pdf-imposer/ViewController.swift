0a0913fbd0ae4dfdfa827d5568f3e32c25772707
Weicheng Zhu <qluoluo@icloud.com>
2016-03-22 08:19:32 -0500

    push

--- Pdf-imposer/ViewController.swift
+++ Pdf-imposer/ViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import Cocoa
+import Quartz
+
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var pdfViewer: PDFView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let url = NSBundle.mainBundle().URLForResource("something", withExtension: "pdf")!
+        let pdf = PDFDocument(URL: url)
+        
+        pdf.allowsPrinting()
+//        print(pdf.pageAtIndex(0).string())
+        let firstPage = CGPDFDocumentCreateWithURL(url)
+        print(url)
+        self.pdfViewer.
     }
 
     override var representedObject: AnyObject? {
